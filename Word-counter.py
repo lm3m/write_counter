91f882bc9864c1713af3f86f0592969d129a455f
Luke W. McCullough - from MacBook <lukaswm@gmail.com>
2015-06-04 22:10:39 -0400

    update menu item on selection and add in selected word count

--- Word-counter.py
+++ Word-counter.py
@@ -4,27 +4,55 @@ from datetime import datetime
 _start_time = None
 _start_count = None
 
-class WordCountCommand(sublime_plugin.TextCommand):
+class StopWordCountCommand(sublime_plugin.TextCommand):
+	def is_visible(self):
+		global _start_time
+		return _start_time is not None
+
 	def run(self, edit):
 		global _start_time
 		global _start_count
-		if not _start_time:
-			_start_time = datetime.utcnow()
-			content = self.view.substr(sublime.Region(0, self.view.size()))
-			_start_count = sum(1 for word in content.split() if any(c.isalpha() for c in word) and len(word) > 2)
-		else:
-			_start_time = None
-			self.view.erase_status("wpm")
-			self.view.erase_status("wc")
+		_start_time = None
+		self.view.erase_status("wpm")
+		self.view.erase_status("wc")
 
+class WordCountCommand(sublime_plugin.TextCommand):
+	def is_visible(self):
+		global _start_time
+		return _start_time is None
+
+	def run(self, edit):
+		global _start_time
+		global _start_count
+		_start_time = datetime.utcnow()
+		content = self.view.substr(sublime.Region(0, self.view.size()))
+		_start_count = sum(1 for word in content.split() if any(c.isalpha() for c in word) and len(word) > 2)
 
 class WordCountEvents(sublime_plugin.EventListener):
+	@staticmethod
+	def _count_words(string):
+		return sum(1 for word in string.split() if any(c.isalpha() for c in word) and len(word) > 2)
+
+	@staticmethod
+	def _count_region(view, region):
+		return WordCountEvents._count_words(view.substr(region))
+
+	def on_selection_modified(self, view):
+		words = 0
+		if len(view.sel()) and _start_time:
+			for selection in view.sel():
+				words += WordCountEvents._count_region(view, selection)
+
+		if words == 0:
+			view.erase_status("selection")
+		else:
+			view.set_status("selection", "SELECTED WORDS: {0}".format(words))
+
 	def on_modified(self, view):
 		global _start_count
 
 		if _start_time:
-			content = view.substr(sublime.Region(0, view.size()))
-			word_count = sum(1 for word in content.split() if any(c.isalpha() for c in word) and len(word) > 2)
+			word_count = WordCountEvents._count_region(view, sublime.Region(0, view.size()))
 			word_count = word_count - _start_count
 
 			view.set_status("wc", "WORD COUNT: {0}".format(word_count))
